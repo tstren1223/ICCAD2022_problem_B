6b09810f1010107d689471e175cd3bdb14d1fda9
tstren1223 <64294878+tstren1223@users.noreply.github.com>
2022-06-18 21:05:51 +0800

    Add files via upload

---
+++ project/bin/placed.pl
@@ -0,0 +1,11 @@
+UCLA pl 1.0
+# User   : Chinese University of Hong Kong
+
+	C1                                                               102      20 : N
+	C2                                                               109      20 : N
+	C3                                                               167      20 : N
+	C4                                                               199      20 : N
+	C5                                                               213      20 : N
+	C6                                                               183      20 : N
+	C7                                                               132      20 : N
+	C8                                                                87      20 : N
