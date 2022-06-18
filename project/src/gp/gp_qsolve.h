2ff2383dfad05cb7f59ba18267be279681db19b1
tstren1223 <64294878+tstren1223@users.noreply.github.com>
2022-06-18 21:39:15 +0800

    gp

---
+++ project/src/gp/gp_qsolve.h
@@ -0,0 +1,17 @@
+#ifndef _GP_QSOLVE_H_
+#define _GP_QSOLVE_H_
+
+#include "gp_cg.h"
+
+void lowerBound(
+        double epsilon,
+        double pseudoAlpha,
+        int repeat,
+        LBMode mode,
+        NetModel model,
+        double relax = 0.0,
+        double maxDisp = -1);
+void initLowerBound();
+
+#endif
+
